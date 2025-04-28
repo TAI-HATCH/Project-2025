1b39e34f1836c7f3b5e2d23fa256df19bbaa915f
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-04-28 13:31:21 +0300

    Send request to the DB and receive the list of questions with the certain id

--- quiz.php
+++ quiz.php
@@ -3,4 +3,16 @@
 include "connection.php";
 //http://localhost/project-2025/quiz.php?language-topic=1
 echo "Hello" . " " . $_GET['language-topic'];
+
+$topic_id = $_GET['language-topic'];
+$table_name = "questions";
+
+$stmt = $conn->prepare("SELECT question, form_content FROM $table_name WHERE languages_topic_id = :topic_id;");
+$stmt->bindParam(':topic_id',$topic_id);
+$stmt->execute();
+
+$questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo var_dump($questions);
+
 ?>

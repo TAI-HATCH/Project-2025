35691b7d0e8facc4a9ae26d2ec87961cb5775b9a
Vertti Koivula <vertti.koivula@gmail.com>
2025-05-22 13:29:22 +0300

    Changes:
    Modified styles for a more slender look
    Included log out and admin banner
    Included upload to database (doesn't work)

--- admin-add-language.php
+++ admin-add-language.php
@@ -1,6 +1,8 @@
 <?php 
 
-include_once "sql_query.php"
+include "admin-logout.php";
+
+include_once "sql_query.php";
 
 ?>
 
@@ -24,16 +26,18 @@ include_once "sql_query.php"
 
   <?php include 'header.php'?>
 
+  <?php include 'admin-banner.php' ?>
+
   <section class="root-content">
-     <div class="admin-input">
-            <label for="add-language">Add programming language</label>
-            <input class="admin-input-field" type="text" id="add-language" name="add-language" placeholder="Type the language to add here.">
+     <div class="admin-add-language">
+            <label class="admin-add-language-label" for="add-language">Add programming language</label>
+            <input class="admin-add-language-input-field" type="text" id="add-language" name="add-language" placeholder="Type the language to add here.">
         </div>
 
         <div class="admin-add-language-topic-selection-content">    
                 <label for="admin-add-language-topic-selection-content">Select topics (optional)</label>
                 <div class="admin-add-language-topic-selection">
-    <label for="admin-add-language-topic-selection-list">Topics:</label>
+                <label for="admin-add-language-topic-selection-list">Topics:</label>
     <ul>
         <?php 
 
@@ -43,23 +47,24 @@ include_once "sql_query.php"
 
         <li>
             <input class='checkbox' type='checkbox' 
-                id="topic<?=($topic['topic_id']) ?>"
-                name="topic[]" value="<?=($topic['topic_id']) ?>"> 
+                    id="topic<?=($topic['topic_id']) ?>"
+                    name="topic[]" value="<?=($topic['topic_id']) ?>"> 
                 
-                <!--id: Print ID for this topic;
+                    <!--id: Print ID for this topic;
                     name: collect all selected topics to array topic[];
                     value: define value for checkbox-->
 
-              <label for="topic<?=($topic['topic_id']) ?>"> <!--Link checkbox to topic-->
+                    <label for="topic<?=($topic['topic_id']) ?>"> <!--Link checkbox to topic-->
                   <?=($topic['topic_name']) ?> <!--Print topic-->
-            </label>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-</div>
-
+                    </label>
+                    </li>
+                    <?php endforeach; ?>
+                </ul>
             </div>
-  </section>
+
+        </div>
+    </section>
+    <button type="submit" class="upload-to-database-button">Upload to database</button>
 </body>
 
 </html>

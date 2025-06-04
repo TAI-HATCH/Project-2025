7fd908d212bafcc7d764d81265d67f28fa8b4000
Vertti Koivula <vertti.koivula@gmail.com>
2025-06-04 11:07:07 +0300

    Added about.php

---
+++ about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Welcome to HATCH</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <?php include 'header.php' ?>
+
+    <section class="root-content">
+        <div class="about">
+            <p><strong>HATCH</strong></p>
+            <p>Hatch is a project with the goal of creating a practice space for coding.</p>
+            <div class="about-text">
+                <div class="about-text-p">
+                    <p>Often learning to code can be a struggle with a lot of moving, abstract parts.
+
+                    </p>
+                </div>
+                 <div class="about-text-p">
+                    <p>We wanted to offer an alternative, easy-to-approach way of studying coding.
+                        
+                    </p>
+                </div>
+                <div class="about-text-p">
+                    <p>
+                        The point of the project was to create a simple, efficient web application to hone one's skills.</p>
+                </div>
+                <div class="about-text-p">
+                    <p>The project began as a one person project but soon grew with two people,
+                         giving the project a real startup feel to it.</p>
+                </div>
+                <div class="about-text-p">
+                    <p>
+                        The website is free to use and without registration.
+                    </p>
+                </div>
+                </div>
+                <div class="about-text-jump-in">
+                    <p>Open the hatch and jump in!</p>
+                </div>
+                <div>
+                    <a class="button" href="admin-start.php">To start page</a>
+                </div>
+            
+        </div>
+    </section>
+</body>

bcb1155b33a674b311f78177a3ca97f871749d8a
Vertti Koivula <vertti.koivula@gmail.com>
2025-06-04 12:41:39 +0300

    Added .png img to about.php Added responsive design

--- about.php
+++ about.php
@@ -18,34 +18,32 @@
 
     <section class="root-content">
         <div class="about">
-            <p><strong>HATCH</strong></p>
             <p>Hatch is a project with the goal of creating a practice space for coding.</p>
             <div class="about-text">
                 <div class="about-text-p">
-                    <p>Often learning to code can be a struggle with a lot of moving, abstract parts.
-
-                    </p>
+                    <img src="./images/about-code.png" alt="Brackets">
+                    <p>Often learning to code can be a struggle with a lot of moving, abstract parts.</p>
                 </div>
                  <div class="about-text-p">
-                    <p>We wanted to offer an alternative, easy-to-approach way of studying coding.
-                        
-                    </p>
+                    <img src="./images/about-easy.png" alt="Snap fingers">
+                    <p>We wanted to offer an alternative, easy-to-approach way of studying coding.</p>
                 </div>
                 <div class="about-text-p">
-                    <p>
-                        The point of the project was to create a simple, efficient web application to hone one's skills.</p>
+                    <img src="./images/about-skill.png" alt="Human gear">
+                    <p>The point of the project was to create a simple, efficient web application to hone one's skills.</p>
                 </div>
                 <div class="about-text-p">
-                    <p>The project began as a one person project but soon grew with two people,
-                         giving the project a real startup feel to it.</p>
+                    <img src="./images/about-startup.png" alt="Rocket">
+                    <p>The project began as a one person project but soon grew with two people, 
+                        giving the project a real startup feel to it.</p>
                 </div>
                 <div class="about-text-p">
-                    <p>
-                        The website is free to use and without registration.
-                    </p>
+                    <img src="./images/about-free.png" alt="Kite">
+                    <p>The website is <strong>free to use</strong> and <strong>without registration.</strong></p>
                 </div>
                 </div>
                 <div class="about-text-jump-in">
+                    <img src="./images/about-jump.png" alt="Sledding">
                     <p>Open the hatch and jump in!</p>
                 </div>
                 <div>
